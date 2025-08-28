e3452f79e6f883d4095b4711ed12b7ef5dbfd00d
Duong Nhat Minh <nhatminh.st.04@gmail.com>
2025-08-28 11:13:49 +0700

    feat: tao thu muc du an ban dau

---
+++ src/main/java/com/nminh/hethongquanlythuvien/HeThongQuanLyThuVienApplication.java
@@ -0,0 +1,13 @@
+package com.nminh.hethongquanlythuvien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeThongQuanLyThuVienApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeThongQuanLyThuVienApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nminh/hethongquanlythuvien/HeThongQuanLyThuVienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nminh.hethongquanlythuvien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeThongQuanLyThuVienApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
