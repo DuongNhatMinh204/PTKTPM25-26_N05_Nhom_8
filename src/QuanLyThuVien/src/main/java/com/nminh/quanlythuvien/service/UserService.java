7ce4897d89467cf2883443e80bb7a23e0bf8c465
Duong Nhat Minh <nhatminh.st.04@gmail.com>
2025-09-11 16:56:46 +0700

    add role shipper

--- src/QuanLyThuVien/src/main/java/com/nminh/quanlythuvien/service/UserService.java
+++ src/QuanLyThuVien/src/main/java/com/nminh/quanlythuvien/service/UserService.java
@@ -7,6 +7,6 @@ import com.nminh.quanlythuvien.model.response.UserSignUpResponseDTO;
 
 
 public interface UserService {
-    public UserSignUpResponseDTO signUp(UserSignUpRequestDTO userSignUpRequestDTO);
-    public UserSignInResponseDTO signIn(UserSignInRequestDTO userSignInRequestDTO);
+    UserSignUpResponseDTO signUp(UserSignUpRequestDTO userSignUpRequestDTO);
+    UserSignInResponseDTO signIn(UserSignInRequestDTO userSignInRequestDTO);
 }
